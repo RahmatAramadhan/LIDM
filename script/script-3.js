9b7760d8e55127f57649fa8ae5779653f2f7a5a6
kumara221 <abiyyu.kumara@gmail.com>
2024-10-25 15:13:50 +0700

    materials/jaring-makanan

---
+++ script/script-3.js
@@ -0,0 +1,227 @@
+
+
+const Script = [
+  { start: 0, end: 1.5, text: "Selamat datang kembali" },
+  {
+    start: 2,
+    end: 7.5,
+    text: "Sekarang kita akan melanjutkan perjalanan kita untuk memahami lebih dalam tentang aliran energi di alam melalui Rantai Makanan.",
+  },
+  {
+    start: 9,
+    end: 10.5,
+    text: "Yuk, kita mulai! ",
+  },
+  {
+    start: 11,
+    end: 12,
+    text: "Produsen",
+  },
+  {
+    start: 12.5,
+    end: 16,
+    text: "Setiap makhluk hidup membutuhkan energi untuk menjalankan kehidupan",
+  },
+  {
+    start: 17,
+    end: 20,
+    text: "Energi ini bermula dari produsen (tumbuhan hijau)",
+  },
+  {
+    start: 21,
+    end: 26.5,
+    text: "Tumbuhan mendapatkan energi dari cahaya matahari dan mengubahnya menjadi energi kimia melalui proses fotosintesis",
+  },
+  {
+    start: 28,
+    end: 31,
+    text: "Mereka adalah sumber energi pertama dalam rantai makanan",
+  },
+  {
+    start: 32,
+    end: 37,
+    text: "Bayangkan jika tidak ada tumbuhan, apa yang akan terjadi pada semua makhluk hidup yang bergantung padanya?",
+  },
+  {
+    start: 37,
+    end: 38,
+    text: "Konsumen 1",
+  },
+  {
+    start: 40,
+    end: 41,
+    text: "Setelah tumbuhan,",
+  },
+  {
+    start: 41,
+    end: 43,
+    text: "peran berikutnya dalam rantai makanan adalah konsumen I",
+  },
+  {
+    start: 43,
+    end: 47,
+    text: "Konsumen I adalah hewan yang mendapatkan energi langsung dari produsen",
+  },
+  {
+    start: 48,
+    end: 53,
+    text: "Di sini, belalang berperan sebagai konsumen I karena ia memakan daun dari tumbuhan",
+  },
+  {
+    start: 54,
+    end: 57.5,
+    text: "Energi yang tersimpan dalam daun berpindah ke tubuh belalang saat ia memakannya",
+  },
+  {
+    start: 58,
+    end: 60,
+    text: "Konsumen 2",
+  },
+  {
+    start: 61,
+    end: 63,
+    text: "Sekarang, mari kita beralih ke konsumen II",
+  },
+  {
+    start: 64,
+    end: 68,
+    text: "Konsumen II adalah hewan yang memakan konsumen I untuk mendapatkan energi",
+  },
+  {
+    start: 69,
+    end: 71,
+    text: "Misalnya, katak yang memakan belalang",
+  },
+  {
+    start: 71,
+    end: 78,
+    text: "Energi yang awalnya diserap oleh tumbuhan kemudian berpindah melalui belalang dan sekarang ke dalam tubuh katak",
+  },
+  {
+    start: 79,
+    end: 82,
+    text: "Proses ini menunjukkan bagaimana energi mengalir ke atas dalam rantai makanan.",
+  },
+  {
+    start: 83 ,
+    end: 85,
+    text: "Konsumen 3",
+  },
+  {
+    start: 85,
+    end: 87,
+    text: "Berikutnya konsumen 3",
+  },
+  {
+    start: 88,
+    end: 91,
+    text: "Konsumen III memakan konsumen II untuk mendapatkan energi",
+  },
+  {
+    start: 92,
+    end: 95,
+    text: "Di sini, ular adalah konsumen III yang memangsa katak",
+  },
+  {
+    start: 96,
+    end: 104,
+    text: "Pada tahap ini, energi yang tadinya berasal dari tumbuhan, kemudian ke belalang, lalu ke katak, kini berpindah lagi ke ular",
+  },
+  {
+    start: 105,
+    end: 113,
+    text: "Semakin tinggi tingkat trofik, semakin sedikit energi yang berpindah karena sebagian besar energi hilang dalam bentuk panas atau tersimpan dalam bagian yang tidak dimakan",
+  },
+  {
+    start: 114,
+    end: 115,
+    text: "Konsumen puncak",
+  },
+  {
+    start: 116,
+    end: 119,
+    text: "Dan akhirnya, kita sampai pada konsumen puncak",
+  },
+  {
+    start: 119,
+    end: 122,
+    text: "Elang adalah predator di puncak rantai makanan ini",
+  },
+  {
+    start: 123,
+    end: 127,
+    text: "Sebagai konsumen puncak, elang mendapatkan energinya dengan memangsa ular",
+  },
+  {
+    start: 128,
+    end: 134,
+    text: "Meskipun elang berada di puncak rantai makanan, hanya sebagian kecil energi dari tumbuhan yang sampai ke tubuhnya",
+  },
+  {
+    start: 134,
+    end: 140,
+    text: "Ini menunjukkan bahwa semakin tinggi tingkat trofik, semakin kecil jumlah energi yang bisa dimanfaatkan",
+  },
+  {
+    start: 141,
+    end: 152,
+    text: "Dekomposer",
+  },
+  {
+    start: 153,
+    end: 156,
+    text: "Tapi, apa yang terjadi setelah organisme mati ?",
+  },
+  {
+    start: 157,
+    end: 160,
+    text: "Di sinilah peran dekomposer, seperti bakteri dan jamur dimulai",
+  },
+  {
+    start: 161,
+    end: 167,
+    text: "Mereka menguraikan sisa-sisa organisme mati menjadi senyawa anorganik yang kemudian diserap kembali oleh tumbuhan.",
+  },
+  {
+    start: 168,
+    end: 177,
+    text: "Ini adalah bagian penting dari siklus alam, di mana tidak ada yang benar-benar terbuang, dan bahan-bahan anorganik bisa digunakan kembali oleh produsen untuk melanjutkan proses fotosintesis",
+  },
+  {
+    start: 178,
+    end: 181,
+    text: "Faktanya, rantai makanan tidak berdiri sendiri.",
+  },
+  {
+    start: 182,
+    end: 186,
+    text: "Di alam, banyak rantai makanan yang saling terkait dan membentuk jaring-jaring makanan",
+  },
+  {
+    start: 187,
+    end: 194,
+    text: "Misalnya, belalang yang tadi dimakan katak mungkin juga dimakan oleh burung kecil, atau katak itu sendiri mungkin memangsa serangga lain selain belalang",
+  },
+  {
+    start: 195,
+    end: 201,
+    text: "Semakin kompleks jaring-jaring makanan di suatu ekosistem, semakin tinggi tingkat kestabilannya",
+  },
+  {
+    start: 201.5,
+    end: 205.5,
+    text: "Bayangkan jika salah satu makhluk hidup hilang, jaring ini bisa menjadi goyah",
+  },
+  {
+    start: 206.6,
+    end: 212.5,
+    text: "DItulah pembelajaran kita kali ini tentang rantai makanan dan bagaimana energi mengalir dari satu organisme ke organisme lainnya",
+  },
+  {
+    start: 213,
+    end: 119,
+    text: "Mulai dari produsen hingga konsumen puncak, serta peran penting dekomposer yang menjaga siklus tetap berjalan",
+  },
+
+  { start: 220, end: 227, text: "Semakin kita memahami rantai ini, semakin kita bisa menghargai peran setiap makhluk hidup dalam menjaga keseimbangan alam" },
+];
